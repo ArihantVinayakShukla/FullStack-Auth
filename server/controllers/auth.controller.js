25cc7a3fc5b338d39a659bbeb33bbef413900d1b
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2024-06-28 21:49:00 +0530

    created signUp api route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signUp = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      res.status(400).json({ msg: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered succesfully",
+      user: {
+        _id: newUser._id,
+        name: newUser.username,
+        email: newUser.email
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -19,6 +19,6 @@ const userSchema = new Schema({
     }
 }, { timestamps: true });
 
-const User = mongoo.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
