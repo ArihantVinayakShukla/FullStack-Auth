0ae3944cb3f85950b02a10dbc21b253430c8017a
ArihantVinayakShukla <shuklaarihant0@gmail.com>
2024-07-03 21:54:35 +0530

    added the update user api route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,1 +1,30 @@
-export const test = (req, res) => {};
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.user._id !== req.params.id) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.passqord, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/user", userRoutes);
